9c7378127b2a422d0a8e785d7b40610b7036b430
Suhas Kashyap <kashyapsuhas07@gmail.com>
2021-09-06 05:29:54 +0530

    Add work page Resume

--- pages/work.js
+++ pages/work.js
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
 import SideTitle from "../components/SideTitle";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Work = ({ className }) => {
+  const pdfFile = "/Resume.pdf";
+
   return (
     <main className={`${className} relative md:mt-6`}>
       <SideTitle>/work</SideTitle>
 
-      <MaxWidthWrapper>
-        <div className="flex text-xl">Work page under construction</div>
+      <MaxWidthWrapper className="p-5 flex justify-center items-center">
+        <Link href="/Resume.pdf">
+          <a target="_blank" className="overflow-scroll border border-gray-200">
+            <Document file={pdfFile}>
+              <Page pageNumber={1} />
+            </Document>
+          </a>
+        </Link>
       </MaxWidthWrapper>
     </main>
   );
