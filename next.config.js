8d876db190fc49de3205e25ed8cc5ff312752220
Suhas Kashyap <kashyapsuhas07@gmail.com>
2023-03-08 23:50:43 +0530

    allow all images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'scontent.cdninstagram.com',
-      'scontent-iad3-1.cdninstagram.com',
-      'scontent-iad3-2.cdninstagram.com',
-      'scontent-iad3-3.cdninstagram.com',
-      'scontent-iad3-4.cdninstagram.com',
+    // domains: [
+    //   'scontent.cdninstagram.com',
+    //   'scontent-iad3-1.cdninstagram.com',
+    //   'scontent-iad3-2.cdninstagram.com',
+    //   'scontent-iad3-3.cdninstagram.com',
+    //   'scontent-iad3-4.cdninstagram.com',
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
