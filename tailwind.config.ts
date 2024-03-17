6bb7eca8471a150585e742bb49f1210ec6c2a4cb
kashyap07 <kashyapsuhas07@gmail.com>
2024-03-18 02:54:10 +0530

    1 binaries uploaded

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,49 +1,51 @@
-import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import typography from '@tailwindcss/typography';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
       dropShadow: {},
       textShadow: {
-        "neon-glow--primary": "var(--neon-glow-primary)",
-        "neon-glow--secondary": "var(--neon-glow-secondary)",
-        "white-glow": "var(--white-glow)",
+        'neon-glow--primary': 'var(--neon-glow-primary)',
+        'neon-glow--secondary': 'var(--neon-glow-secondary)',
+        'white-glow': 'var(--white-glow)',
       },
     },
   },
   plugins: [
+    typography,
     plugin(function ({ addVariant }) {
-      addVariant("children-iterative", "& *");
-      addVariant("children", "& > *");
-      addVariant("optional", "&:optional");
-      addVariant("hocus", ["&:hover", "&:focus"]);
-      addVariant("supports-grid", "@supports (display: grid)");
+      addVariant('children-iterative', '& *');
+      addVariant('children', '& > *');
+      addVariant('optional', '&:optional');
+      addVariant('hocus', ['&:hover', '&:focus']);
+      addVariant('supports-grid', '@supports (display: grid)');
     }),
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          "text-shadow": (value) => ({
+          'text-shadow': (value) => ({
             textShadow: value,
           }),
         },
-        { values: theme("textShadow") }
+        { values: theme('textShadow') }
       );
     }),
   ],
