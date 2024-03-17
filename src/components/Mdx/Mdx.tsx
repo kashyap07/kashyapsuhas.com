8c0d7f064595322d6a08a0cc1ef09ad97444f215
kashyap07 <kashyapsuhas07@gmail.com>
2024-03-18 03:29:40 +0530

    image auto height

--- src/components/Mdx/Mdx.tsx
+++ src/components/Mdx/Mdx.tsx
@@ -1,10 +1,10 @@
 // @ts-nocheck
 
-import Link from "next/link";
-import Image from "next/image";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import { highlight } from "sugar-high";
-import React from "react";
+import Link from 'next/link';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { highlight } from 'sugar-high';
+import React from 'react';
+import ImageAutoHeight from '../ImageAutoHeight';
 
 // FIXME: add types
 // FIXME: images where
@@ -34,7 +34,7 @@ function Table({ data }) {
 function CustomLink(props) {
   let href = props.href;
 
-  if (href.startsWith("/")) {
+  if (href.startsWith('/')) {
     return (
       <Link href={href} {...props}>
         {props.children}
@@ -42,7 +42,7 @@ function CustomLink(props) {
     );
   }
 
-  if (href.startsWith("#")) {
+  if (href.startsWith('#')) {
     return <a {...props} />;
   }
 
@@ -68,33 +68,29 @@ function slugify(str) {
     .toString()
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
-    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-'); // Replace multiple - with single -
 }
 
 function createHeading(level) {
   return ({ children }) => {
     let slug = slugify(children);
-    
+
     return React.createElement(
       `h${level}`,
       { id: slug },
       [
-        React.createElement("a", {
+        React.createElement('a', {
           href: `#${slug}`,
           key: `link-${slug}`,
-          className: "anchor",
+          className: 'anchor',
         }),
       ],
       children
     );
   };
-}
-
-function RoundedImage(props) {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
 }
 
 let components = {
@@ -104,7 +100,7 @@ let components = {
   h4: createHeading(4),
   h5: createHeading(5),
   h6: createHeading(6),
-  Image: RoundedImage,
+  Image: ImageAutoHeight,
   a: CustomLink,
   Callout,
   code: Code,
