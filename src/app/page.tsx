4c51752abdb8774a4d19e27cd933184d7bb1df48
Suhas <kashyapsuhas07@gmail.com>
2024-10-28 07:18:09 +0530

    ugly code but i like this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,69 +9,114 @@ export const metadata = {
   description: "Welcome to Suhas Kashyap's slice of the interwebs.",
 };
 
-const BGImg = () => (
-  <div className="absolute">
-    <div className="aspect-[1/2] w-screen overflow-hidden">
-      <Image
-        src="/suhas_hike_2.jpg"
-        alt="suhas kashyap hiking"
-        fill={true}
-        sizes=""
-        priority
-        quality={100}
-        className="imageMask top-0 origin-bottom-left transform object-cover md:!left-1/2 md:max-w-[40rem] md:-translate-x-1/2"
-      />
-    </div>
+const PreFold_SP = () => (
+  <div className="sm:hidden">
+    <Wrapper
+      maxWidth={MaxWidth.FullWidth}
+      className="flex h-screen min-h-[880px] px-0 sm:hidden"
+    >
+      {/* SP bg image begin */}
+      <div className="absolute aspect-[1/2] w-screen overflow-hidden sm:hidden">
+        <Image
+          src="/suhas_hike.jpg"
+          alt="suhas kashyap hiking"
+          fill={true}
+          sizes=""
+          priority
+          quality={100}
+          className="imageMask_SP top-0 origin-bottom-left transform object-cover"
+        />
+      </div>
+      {/* SP bg image end */}
 
-    <div className="navbarHeight fixed w-lvw overflow-hidden bg-black" />
-  </div>
-);
-
-const PreFold = () => (
-  <Wrapper maxWidth={MaxWidth.FullWidth} className="px-0 h-screen min-h-[880px]">
-    <BGImg/>
+      <Wrapper
+        maxWidth={MaxWidth.Narrow}
+        className="relative flex flex-col justify-end py-32"
+      >
+        <span className="text-2xl drop-shadow-sm">Hi. I&apos;m</span>
+        <h1 className="mb-2 text-7xl leading-[0.9]">Suhas Kashyap</h1>
+        <span className="mb-8 text-2xl">
+          Welcome to my slice of the Interwebs.
+        </span>
+      </Wrapper>
+    </Wrapper>
 
     <Wrapper
       maxWidth={MaxWidth.Narrow}
-      className="textShadow relative flex flex-col justify-end py-32"
+      className="flex flex-col justify-center pb-32"
     >
-      <span className="text-2xl drop-shadow-sm">Hi. I&apos;m</span>
-      <h1 className="mb-2 text-7xl leading-[0.9]">Suhas Kashyap</h1>
-      <span className="mb-2 text-2xl">
-        Welcome to my slice of the Interwebs.
-      </span>
+      <h2 className="text-2xl">Links</h2>
+      <ul className="flex flex-col gap-4">
+        <li className="flex items-center justify-between text-4xl">
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li className="flex items-center justify-between text-4xl">
+          <Link href={"/photos"}>Photos</Link>
+        </li>
+        <li className="flex items-center justify-between text-4xl">
+          <Link href={"/resume"}>Resume</Link>
+        </li>
+        <li className="flex items-center justify-between text-4xl">
+          <Link href={"/contact"}>Contact</Link>
+        </li>
+      </ul>
     </Wrapper>
-  </Wrapper>
+  </div>
 );
 
-const PostFold = () => (
+const PreFold_PC = () => (
   <Wrapper
     maxWidth={MaxWidth.Narrow}
-    className="flex flex-col justify-center pb-32"
+    className="hidden h-screen w-full items-center justify-between px-0 py-32 sm:flex"
   >
-    <h2 className="text-2xl">Links</h2>
-    <ul className="flex flex-col gap-4">
-      <li className="flex items-center justify-between text-4xl">
-        <Link href={"/blog"}>Blog</Link>
-      </li>
-      <li className="flex items-center justify-between text-4xl">
-        <Link href={"/photos"}>Photos</Link>
-      </li>
-      <li className="flex items-center justify-between text-4xl">
-        <Link href={"/resume"}>Resume</Link>
-      </li>
-      <li className="flex items-center justify-between text-4xl">
-        <Link href={"/contact"}>Contact</Link>
-      </li>
-    </ul>
+    <div className="flex flex-col gap-36">
+      <div className="relative flex flex-col justify-center">
+        <span className="text-2xl drop-shadow-sm">Hi. I&apos;m</span>
+        <h1 className="mb-2 text-7xl leading-[0.9]">Suhas Kashyap</h1>
+        <span className="mb-8 text-2xl">
+          Welcome to my slice of the Interwebs.
+        </span>
+      </div>
+
+      <div className="flex flex-col justify-center">
+        <h2 className="mb-2 text-2xl">Links</h2>
+        <ul className="flex flex-col gap-4">
+          <li className="flex items-center justify-between text-4xl">
+            <Link href={"/blog"}>Blog</Link>
+          </li>
+          <li className="flex items-center justify-between text-4xl">
+            <Link href={"/photos"}>Photos</Link>
+          </li>
+          <li className="flex items-center justify-between text-4xl">
+            <Link href={"/resume"}>Resume</Link>
+          </li>
+          <li className="flex items-center justify-between text-4xl">
+            <Link href={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className="">
+      <Image
+        src="/suhas_nobg.png"
+        alt="suhas kashyap hiking"
+        height={500}
+        width={500}
+        sizes=""
+        priority
+        quality={100}
+        className="imageMask_PC top-0 origin-bottom-left transform object-cover"
+      />
+    </div>
   </Wrapper>
 );
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between font-semibold">
-      <PreFold />
-      <PostFold />
+      <PreFold_SP />
+      <PreFold_PC />
     </main>
   );
 }
