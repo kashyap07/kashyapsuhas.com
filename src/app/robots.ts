404a649c946e0570e205349ffca6efbd67261a06
kashyap07 <kashyapsuhas07@gmail.com>
2025-05-29 00:56:19 +0530

    add work to robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      disallow: ["/suhas-resume.pdf", "/_next/*", "/contact"],
+      disallow: ["/suhas-resume.pdf", "/_next/*", "/contact", "/work"],
     },
   };
 }
