e48eec0d1de887aca5bc036614ed4f146c16861d
kashyap07 <kashyapsuhas07@gmail.com>
2025-06-16 02:53:15 +0530

    change 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+import { Wrapper } from "@/components/Wrapper";
+import cn from "@/utils/cn";
+
+export default function NotFound() {
+  return (
+    <Wrapper className="flex h-screen max-h-screen w-full flex-col items-center justify-center text-center">
+      <h1 className="flex flex-col gap-0 text-[10rem] font-medium">
+        <span>404</span>
+      </h1>
+      <Link href="/" className={cn("text-5xl font-bold text-columbiaYellow")}>
+        YOU'RE LOST. GO HOME.
+      </Link>
+    </Wrapper>
+  );
+}
