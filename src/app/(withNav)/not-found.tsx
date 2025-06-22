9a7cabb12c015212a2dc95674c2548aa1b987f84
kashyap07 <kashyapsuhas07@gmail.com>
2025-06-22 23:47:11 +0530

    ez 404

--- src/app/(withNav)/not-found.tsx
+++ src/app/(withNav)/not-found.tsx
@@ -1,17 +1,5 @@
-import Link from "next/link";
-
-import { Wrapper } from "@/components/ui";
-import cn from "@/utils/cn";
+import { notFound } from "next/navigation";
 
 export default function NotFound() {
-  return (
-    <Wrapper className="flex h-screen max-h-screen w-full flex-col items-center justify-start py-12 text-center">
-      <h1 className="flex flex-col gap-0 text-[10rem] font-medium">
-        <span>404</span>
-      </h1>
-      <Link href="/" className={cn("text-5xl font-bold text-columbiaYellow")}>
-        YOU'RE LOST. GO HOME.
-      </Link>
-    </Wrapper>
-  );
+  return notFound();
 }
