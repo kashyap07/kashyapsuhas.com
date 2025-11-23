4ebfcfa7dd0d3233647c681ca6d6caf5a1e8c74e
kashyap07 <kashyapsuhas07@gmail.com>
2025-11-23 15:55:58 +0530

    wrap reviews with suspense

--- src/app/(withNav)/reviews/page.tsx
+++ src/app/(withNav)/reviews/page.tsx
@@ -1,8 +1,14 @@
+import { Suspense } from "react";
+
 import { getReviews } from "@db/reviews";
 
 import Reviews from "./Reviews";
 
 export default function ReviewsPage() {
   const reviews = getReviews();
-  return <Reviews reviews={reviews} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Reviews reviews={reviews} />
+    </Suspense>
+  );
 }
