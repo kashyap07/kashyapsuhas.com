997bdcf6efa2fe5e9b9670ef849e6e90a2b10b53
kashyap07 <kashyapsuhas07@gmail.com>
2026-01-15 17:49:14 +0530

    add umami, update license

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Eczar } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -19,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={eczar.className}>{children}</body>
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="5f57cfa4-f6f0-4820-81ac-6bf61facd981"
+      />
     </html>
   );
 }
