2cdc2d7096ac4df583f2aec3e9f5df98f69c34fa
kashyap07 <kashyapsuhas07@gmail.com>
2026-01-17 16:49:31 +0530

    remove robots metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,17 +52,6 @@ export const metadata: Metadata = {
     lastName: "Kashyap",
     username: "kashyap07",
   },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
 };
 
 const NavLinks = () => (
